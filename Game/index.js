fff968a7b78af39a0a0f4e0fc18f598ec340a943
cyrisek <cyrisek07@gmail.com>
2020-04-08 23:16:31 +0000

    early JS development

--- Game/index.js
+++ Game/index.js
@@ -1,1 +1,10 @@
 console.log('here');
+
+    function onCardClicked(e) {
+        const target = e.currentTarget;
+        console.log(target.className);
+        target.className = target.className
+        .replace('color-hidden', '')
+        .trim();
+
+    }
