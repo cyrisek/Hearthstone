34f583a85caa9bf0be6be476efcf848bfc5a50e9
cyrisek <cyrisek07@gmail.com>
2020-04-09 20:07:50 +0000

    making things rotate

--- Game/index.js
+++ Game/index.js
@@ -73,18 +73,3 @@ function onCardClicked(e) {
     }
   }
 }
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add('flip');
-
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-
-    return;
-  }
-
-  secondCard = this;
-}
